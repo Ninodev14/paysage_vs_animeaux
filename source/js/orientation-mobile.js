658afdec52ea306f2774e3c60f59b961e49d454f
TheSloum <maelvirgil@gmail.com>
2024-02-09 16:07:26 +0100

    rotatephonemessage

--- source/js/orientation-mobile.js
+++ source/js/orientation-mobile.js
@@ -1,13 +1,8 @@
-function lockOrientation() {
-    if (screen.orientation.lock) {
-      // Lock the screen orientation
-      screen.orientation.lock("portrait-primary").then(function success() {
-        console.log("Orientation locked");
-      }, function error(err) {
-        console.error("Unable to lock orientation: " + err.message);
-      });
-    }
-  }
 
-  // Call the function to lock the orientation when the page loads
-  lockOrientation();
+    window.addEventListener('orientationchange', function() {
+      if (window.orientation === 90 || window.orientation === -90) {
+        document.getElementById('rotateMessage').style.display = 'block';
+      } else {
+        document.getElementById('rotateMessage').style.display = 'none';
+      }
+    });
